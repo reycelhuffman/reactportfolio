35cf8ea0b517833bb94294e33066204cb7db54dd
Reycel Huffman <reycel.huffman@outlook.com>
2022-06-01 22:55:01 -0500

    buttons created

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,20 @@ import Image from "../elements/Image";
 export default function Home() {
   return (
     <div className='backgroundimage'>
+      <a href="https://docs.google.com/document/d/1liyCcq_EasESWtMTfsrcsYcqI-m5U1Nc/edit?usp=sharing&ouid=110244157607918638497&rtpof=true&sd=true">
+        <Image
+          src={require("./../../assets/img/portfoliologo.png")}
+          alt="Github"
+          width={100}
+          href="https://docs.google.com/document/d/1liyCcq_EasESWtMTfsrcsYcqI-m5U1Nc/edit?usp=sharing&ouid=110244157607918638497&rtpof=true&sd=true"
+          height={100}
+        />
+      </a>
       <h1>Home Page</h1>
+      <br />
+      <p>
+            Thank you for checking out my page! Here are my links to my pages! 
+      </p>
       <a href="https://github.com/reycelhuffman">
         <Image
           src={require("./../../assets/img/Octocat.png")}
@@ -23,28 +36,25 @@ export default function Home() {
           height={100}
         />
       </a>
-      <p>
-            Thank you for checking out my page! Here are my links to my pages! 
-      </p>
-      <a href="https://docs.google.com/document/d/1liyCcq_EasESWtMTfsrcsYcqI-m5U1Nc/edit?usp=sharing&ouid=110244157607918638497&rtpof=true&sd=true">
+      <a href="https://github.com/reycelhuffman/reactportfolio/raw/main/reycelresume2022.docx
+">
         <Image
-          src={require("./../../assets/img/portfoliologo.png")}
+          src={require("./../../assets/img/resume.png")}
           alt="Github"
           width={100}
-          href="https://docs.google.com/document/d/1liyCcq_EasESWtMTfsrcsYcqI-m5U1Nc/edit?usp=sharing&ouid=110244157607918638497&rtpof=true&sd=true"
+          href="https://github.com/reycelhuffman/reactportfolio/raw/main/reycelresume2022.docx
+          "
           height={100}
         />
       </a>
+      
       <br />
       <br />
       <br />
       
       <br />
       <br />
-      <a href="https://www.linkedin.com/in/reycel-huffman/">Click for my Linkedin Link</a>
-      <br />
-      <br />
-      <a href="./../../assets/reycelresume2022.docx">Click for my Resume Link</a>
+      
     </div>
   );
 }
